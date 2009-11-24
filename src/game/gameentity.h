3069508d9b2fd09829a1d23ee6e69b926b8685c9
Nathan Samson <nathansamson@gmail.com>
2009-11-24 22:11:58 +0100

    Fix compile errors for game entity

--- src/game/gameentity.h
+++ src/game/gameentity.h
@@ -1,6 +1,8 @@
 #ifndef INC_GAMEENTITY_H
 #define INC_GAMEENTITY_H
 
+#include "misc/vector2.h"
+
 namespace SI {
 
 	/**
@@ -10,11 +12,13 @@ namespace SI {
 	public:
 		virtual void visualize();
 		virtual void update(unsigned int) = 0;
+		
+		Vector2 getLocation();
 	protected:
 		void move(Vector2);
 	private:
 		Vector2 fLocation;
-	}
+	};
 }
 
 #endif // INC_GAMEENTTITY_H
