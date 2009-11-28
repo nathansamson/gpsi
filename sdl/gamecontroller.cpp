9cc432bfb62f19fa85fffa1543a9613b43a818f1
Nathan Samson <nathansamson@gmail.com>
2009-11-28 15:51:09 +0100

    Use the new quit request mechanism

--- sdl/gamecontroller.cpp
+++ sdl/gamecontroller.cpp
@@ -1,11 +1,15 @@
+#include "zabbr/widgets/button.h"
+
 #include "gamecontroller.h"
 #include "sdlentityfactory.h"
 #include "sdlkeyboardinputdriver.h"
 
 namespace SISDL {
-	GameController::GameController(Zabbr::SDLWindow* w): Zabbr::VSDLController(w), fGame(0), fTimeRemainder(0.0) {
+	GameController::GameController(Zabbr::SDLWindow* w): Zabbr::VSDLController(w),
+	                fGame(0), fTimeRemainder(0.0), fClosed(false) {
 		fInputDriver = new SDLKeyboardInputDriver();
 		fGame = new SI::Game(fInputDriver, new SDLEntityFactory(w));
+		connectRequestQuit(new GameControllerEmptyCallback(this, &GameController::onRequestQuitGame));
 	}
 	
 	GameController::~GameController() {
@@ -13,10 +17,102 @@ namespace SISDL {
 	}
 	
 	void GameController::draw() {
+		if (fClosed) {
+			openParentController();
+		}
 		double time = fTimer.reset();
 		int ticks = (time + fTimeRemainder) * 1000;
 		fTimeRemainder = (time + fTimeRemainder) * 1000 - ticks;
 		
 		fGame->update(ticks);
+	}
+	
+	void GameController::keyDown(SDL_KeyboardEvent evnt) {
+		fInputDriver->keyDown(evnt);
+	}
+	
+	void GameController::keyRelease(SDL_KeyboardEvent evnt) {
+		fInputDriver->keyRelease(evnt);
+	}
+	
+	void GameController::onRequestQuitGame() {
+		fQuitConfirmation = new Zabbr::MenuController(fWindow);
+		Zabbr::Button* b = new Zabbr::Button(fWindow, "Resume game");
+		b->connectOnMouseClick(new GameControllerMouseButtonCallback(this, &GameController::onResumeGame));
+		fQuitConfirmation->addWidget(b);
+
+		b = new Zabbr::Button(fWindow, "Quit Game");
+		b->connectOnMouseClick(new GameControllerMouseButtonCallback(this, &GameController::onQuitGame));
+		fQuitConfirmation->addWidget(b);
+		
+		b = new Zabbr::Button(fWindow, "Close game");
+		b->connectOnMouseClick(new GameControllerMouseButtonCallback(this, &GameController::onCloseGame));
+		fQuitConfirmation->addWidget(b);
+
+		background();
+		openController(fQuitConfirmation);
+	}
+	
+	/**
+	 * Resume game callback.
+	*/
+	void GameController::onResumeGame(SDL_MouseButtonEvent e) {
+		fQuitConfirmation->openParentController();
+	}
+	
+	/**
+	 * Quit game callback.
+	*/
+	void GameController::onQuitGame(SDL_MouseButtonEvent e) {
+		fQuitConfirmation->openParentController();
+		fClosed = true;
+		//fController->openParentParentController();
+	}
+	
+	/**
+	 * Close game callback.
+	*/
+	void GameController::onCloseGame(SDL_MouseButtonEvent e) {
+		fQuitConfirmation->quit();
+	}
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param window The SIWindow.
+	 * @param fp The member pointer.
+	*/
+	GameControllerEmptyCallback::GameControllerEmptyCallback(
+	       GameController* c, void (GameController::*fp)())
+	                            : fGameController(c), fFunction(fp) {
+	}
+
+	/**
+	 * Call the callback with specific parameter.
+	*/
+	void GameControllerEmptyCallback::call()
+	{
+		(fGameController->*fFunction)();
+	}
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param window The SIWindow.
+	 * @param fp The member pointer.
+	*/
+	GameControllerMouseButtonCallback::GameControllerMouseButtonCallback(
+	       GameController* c, void (GameController::*fp)(SDL_MouseButtonEvent))
+	                            : fGameController(c), fFunction(fp) {
+	}
+
+	/**
+	 * Call the callback with specific parameter.
+	 *
+	 * @param e The SDL_MouseButtonEvent
+	*/
+	void GameControllerMouseButtonCallback::call(SDL_MouseButtonEvent e)
+	{
+		(fGameController->*fFunction)(e);
 	}
 }
--- sdl/gamecontroller.h
+++ sdl/gamecontroller.h
@@ -1,4 +1,5 @@
 #include "zabbr/controller/sdlcontroller.h"
+#include "zabbr/controller/menucontroller.h"
 #include "zabbr/misc/stopwatch.h"
 #include "src/game/game.h"
 
@@ -9,13 +10,59 @@ namespace SISDL {
 	class GameController: public Zabbr::VSDLController {
 	public:
 		GameController(Zabbr::SDLWindow*);
-		~GameController();
+		virtual ~GameController();
 	
 		virtual void draw();
+		virtual void keyDown(SDL_KeyboardEvent);
+		virtual void keyRelease(SDL_KeyboardEvent);
+		
+		void onRequestQuitGame();
+		void onResumeGame(SDL_MouseButtonEvent);
+		void onQuitGame(SDL_MouseButtonEvent);
+		void onCloseGame(SDL_MouseButtonEvent);
 	private:
 		SI::Game* fGame;
 		SDLInputDriver* fInputDriver;
 		Zabbr::Stopwatch fTimer;
 		double fTimeRemainder;
+		
+		Zabbr::MenuController* fQuitConfirmation;
+		bool fClosed;
+	};
+	
+	/**
+	 * GameController window empty callback.
+	*/
+	class GameControllerEmptyCallback: public Zabbr::IEmptyCallback {
+	public:
+		GameControllerEmptyCallback(GameController*, void (GameController::*)());
+		virtual void call();
+	private:
+		/**
+		 * The attached game controller.
+		*/
+		GameController* fGameController;
+		/**
+		 * The attached member function.
+		*/
+		void (GameController::* fFunction)();
+	};
+	
+	/**
+	 * GameController window mouse button callback.
+	*/
+	class GameControllerMouseButtonCallback: public Zabbr::IMouseButtonCallback {
+	public:
+		GameControllerMouseButtonCallback(GameController*, void (GameController::*)(SDL_MouseButtonEvent));
+		virtual void call(SDL_MouseButtonEvent);
+	private:
+		/**
+		 * The attached game controller.
+		*/
+		GameController* fGameController;
+		/**
+		 * The attached member function.
+		*/
+		void (GameController::* fFunction)(SDL_MouseButtonEvent);
 	};
 }
