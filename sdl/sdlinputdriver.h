8cd69c54c7948f3afee5ae8fcb268ef7a3064e29
Nathan Samson <nathansamson@gmail.com>
2009-11-28 15:48:48 +0100

    Basic keyboard input

--- sdl/sdlinputdriver.h
+++ sdl/sdlinputdriver.h
@@ -1,10 +1,15 @@
 #ifndef SDL_INPUTDRIVER_H
 #define SDL_INPUTDRIVER_H
 
+#include "SDL.h"
+
 #include "src/game/shipdriver.h"
 
 namespace SISDL {
 	class SDLInputDriver: public SI::VShipDriver {
+	public:
+		virtual void keyDown(SDL_KeyboardEvent) = 0;
+		virtual void keyRelease(SDL_KeyboardEvent) = 0;
 	};
 }
 
--- sdl/sdlkeyboardinputdriver.cpp
+++ sdl/sdlkeyboardinputdriver.cpp
@@ -1,8 +1,43 @@
+#include <cmath>
 #include "sdlkeyboardinputdriver.h"
 
 namespace SISDL {
 
 	void SDLKeyboardInputDriver::update(int ticks) {
-		move(SI::Vector2(0.000001*ticks, 0.0));
+		const double max = 0.00003;
+		
+		if ((fLeft && fRight) || !(fLeft || fRight)) {
+			fSpeed *= 0;
+		} else if (fLeft) {
+			fSpeed -= SI::Vector2(max/500, 0.0);
+		} else if (fRight) {
+			fSpeed += SI::Vector2(max/500, 0.0); 
+		}
+		
+		if (std::fabs(fSpeed.getX()) > max) {
+			if (fLeft) {
+				fSpeed.setX(-max);
+			} else {
+				fSpeed.setX(max);
+			}
+		}
+		
+		move(fSpeed * ticks);
+	}
+	
+	void SDLKeyboardInputDriver::keyDown(SDL_KeyboardEvent evnt) {
+		if (evnt.keysym.sym == SDLK_LEFT) {
+			fLeft = true;
+		} else if (evnt.keysym.sym == SDLK_RIGHT) {
+			fRight = true;
+		}
+	}
+	
+	void SDLKeyboardInputDriver::keyRelease(SDL_KeyboardEvent evnt) {
+		if (evnt.keysym.sym == SDLK_LEFT) {
+			fLeft = false;
+		} else if (evnt.keysym.sym == SDLK_RIGHT) {
+			fRight = false;
+		}
 	}
 }
