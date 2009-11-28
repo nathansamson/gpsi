aa2d23bc050fabf373d48739cb0c5d0a572e9853
Nathan Samson <nathansamson@gmail.com>
2009-11-28 15:48:48 +0100

    Basic keyboard input

--- sdl/sdlkeyboardinputdriver.h
+++ sdl/sdlkeyboardinputdriver.h
@@ -7,6 +7,13 @@ namespace SISDL {
 	class SDLKeyboardInputDriver: public SDLInputDriver {
 	public:
 		virtual void update(int);
+		virtual void keyDown(SDL_KeyboardEvent);
+		virtual void keyRelease(SDL_KeyboardEvent);
+	private:
+		bool fLeft;
+		bool fRight;
+		
+		SI::Vector2 fSpeed;
 	};
 }
 
