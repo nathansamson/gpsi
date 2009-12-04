5f7d700103c210d9b63987e66f97229b1c13acad
Nathan Samson <nathansamson@gmail.com>
2009-12-04 15:25:00 +0100

    Fix sync drivers + test it

---
+++ src/game/tests/synchronousdrivertest.cpp
@@ -0,0 +1,41 @@
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestSuite.h>
+
+#include "synchronousdrivertest.h"
+
+namespace SITest {
+	#define SYNCDRIVER_TEST(name) new CppUnit::TestCaller<SynchronousDriverTest>( \
+	                                   #name, \
+	                                   &SynchronousDriverTest::name) \
+	
+	void SynchronousDriverTest::setUp() {
+		fDriver = new SI::SynchronousDriver(2.0);
+		fShip = new SITest::MockShip(fDriver, SI::Vector2(0.0, 0.0));
+	}
+	
+	void SynchronousDriverTest::tearDown() {
+		delete fShip;
+	}
+	
+	void SynchronousDriverTest::testMoveNormal() {
+		fShip->update(10);
+		SITEST_ASSERT_VECTORS_EQUAL(Vector2(0.002, -0.001), fShip->getPosition());
+		fShip->update(10000-10);
+		SITEST_ASSERT_VECTORS_EQUAL(Vector2(2.0, -1.0), fShip->getPosition());
+		fShip->update(10);
+		SITEST_ASSERT_VECTORS_EQUAL(Vector2(1.998, -1.001), fShip->getPosition());
+		fShip->update(10000-10);
+		SITEST_ASSERT_VECTORS_EQUAL(Vector2(0.00, -2.000), fShip->getPosition());
+		fShip->update(10000+10);
+		SITEST_ASSERT_VECTORS_EQUAL(Vector2(-1.998, -3.001), fShip->getPosition());
+		fShip->update(20000);
+		SITEST_ASSERT_VECTORS_EQUAL(Vector2(1.998, -5.001), fShip->getPosition());
+	}
+	
+	CppUnit::Test* SynchronousDriverTest::suite() {
+		CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite("Synchronous driver test");
+		suiteOfTests->addTest(SYNCDRIVER_TEST(testMoveNormal));
+		return suiteOfTests;
+	}
+}
--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -3,13 +3,14 @@
 #include "misc/tests/vector2test.h"
 #include "game/tests/shiptest.h"
 #include "game/tests/gametest.h"
+#include "game/tests/synchronousdrivertest.h"
 
 int main(int argc, char* argv[]) {
 	CppUnit::TextTestRunner runner;
 	runner.addTest(SITest::Vector2Test::suite());
 	runner.addTest(SITest::ShipTest::suite());
 	runner.addTest(SITest::GameTest::suite());
-	//runner.addTest(sitest::Vector2::suite() );
+	runner.addTest(SITest::SynchronousDriverTest::suite());
 	runner.run();
 	return 0;
 }
