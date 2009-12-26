9f88557e00a57b981810bc65452a17573017b26e
Nathan Samson <nathansamson@gmail.com>
2009-12-26 14:59:29 +0100

    Add attribute to score entries if they are new or not (and color them if they are in the SDL version)

--- src/misc/highscores.h
+++ src/misc/highscores.h
@@ -23,15 +23,52 @@ namespace SI {
 		typedef std::pair<std::string, int> ScoreEntry;
 		
 		/**
+		 * An updated score entry.
+		*/
+		class UpdatedScoreEntry {
+		public:
+			UpdatedScoreEntry();
+			UpdatedScoreEntry(std::string, int, bool);
+			
+			bool operator==(const UpdatedScoreEntry& o);
+			
+			std::string getName();
+			int getScore();
+			bool isUpdated();
+			
+			
+		private:
+			/**
+			 * Name of the entry-holder.
+			*/
+			std::string fName;
+			
+			/**
+			 * The score of the entry.
+			*/
+			int fScore;
+		
+			/**
+			 * Flag to see if the entry is updated.
+			*/
+			bool fUpdated;
+		};
+		
+		/**
 		 * A map of highscores, the first parameter is the position in the list, the second is the scoreentry.
 		*/
 		typedef std::map<unsigned int, ScoreEntry> HighscoreList;
+		
+		/**
+		 * A map of highscores, the first parameter is the position in the list, the second is the updated scoreentry.
+		*/
+		typedef std::map<unsigned int, UpdatedScoreEntry> UpdatedHighscoreList;
 	
 		Highscores(std::string, unsigned int);
 		~Highscores();
 		
 		bool isHighscore(int);
-		HighscoreList setHighscore(std::string, int, unsigned int, unsigned int);
+		UpdatedHighscoreList setHighscore(std::string, int, unsigned int, unsigned int);
 		HighscoreList getBestHighscores(int);
 		void save(std::string);
 	private:
--- src/misc/tests/highscorestest.cpp
+++ src/misc/tests/highscorestest.cpp
@@ -12,6 +12,7 @@ namespace SITest {
 		                               &HighscoresTest::name) \
 
 	#define HENTRY(name, score) std::pair<std::string, int>(name, score)
+	#define HUENTRY(name, score, updated) Highscores::UpdatedScoreEntry(name, score, updated)
 
 	void HighscoresTest::setUp() {
 	}
@@ -65,95 +66,95 @@ namespace SITest {
 		Highscores highScores("misc/tests/highscores/10items.high", 20);
 		
 		// We say we have place for 10 items (5 best + 2 * 2 contect + 1 new)
-		Highscores::HighscoreList list = highScores.setHighscore("New score", 750, 2, 5);
+		Highscores::UpdatedHighscoreList list = highScores.setHighscore("New score", 750, 2, 5);
 		CPPUNIT_ASSERT(list.size() == 10);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan2", 8000) == list[2]);	
-		CPPUNIT_ASSERT(HENTRY("Nathan3", 7000) == list[3]);
-		CPPUNIT_ASSERT(HENTRY("Nathan4", 6000) == list[4]);
-		CPPUNIT_ASSERT(HENTRY("Nathan5", 5000) == list[5]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan9", 1000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("New score", 750) == list[10]);
-		CPPUNIT_ASSERT(HENTRY("Nathan10", 500) == list[11]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan2", 8000, false) == list[2]);	
+		CPPUNIT_ASSERT(HUENTRY("Nathan3", 7000, false) == list[3]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan4", 6000, false) == list[4]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan5", 5000, false) == list[5]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan9", 1000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("New score", 750, true) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan10", 500, false) == list[11]);
 		
 		list = highScores.setHighscore("Best score", 20050, 2, 5);
 		CPPUNIT_ASSERT(list.size() == 10);
-		CPPUNIT_ASSERT(HENTRY("Best score", 20050) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[2]);
-		CPPUNIT_ASSERT(HENTRY("Nathan2", 8000) == list[3]);
-		CPPUNIT_ASSERT(HENTRY("Nathan3", 7000) == list[4]);
-		CPPUNIT_ASSERT(HENTRY("Nathan4", 6000) == list[5]);
-		CPPUNIT_ASSERT(HENTRY("Nathan5", 5000) == list[6]);
-		CPPUNIT_ASSERT(HENTRY("Nathan6", 4000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("Nathan9", 1000) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Best score", 20050, true) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[2]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan2", 8000, false) == list[3]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan3", 7000, false) == list[4]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan4", 6000, false) == list[5]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan5", 5000, false) == list[6]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan6", 4000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan9", 1000, false) == list[10]);
 		
 		list = highScores.setHighscore("Middle score", 3500, 2, 2);
 		CPPUNIT_ASSERT(list.size() == 7);
-		CPPUNIT_ASSERT(HENTRY("Best score", 20050) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[2]);
-		CPPUNIT_ASSERT(HENTRY("Nathan5", 5000) == list[6]);
-		CPPUNIT_ASSERT(HENTRY("Nathan6", 4000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Middle score", 3500) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Best score", 20050, false) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[2]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan5", 5000, false) == list[6]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan6", 4000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Middle score", 3500, true) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[10]);
 		
 		highScores = Highscores("misc/tests/highscores/10items.high", 10);
 		list = highScores.setHighscore("New score", 750, 2, 3);
 		CPPUNIT_ASSERT(list.size() == 8);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan2", 8000) == list[2]);	
-		CPPUNIT_ASSERT(HENTRY("Nathan3", 7000) == list[3]);
-		CPPUNIT_ASSERT(HENTRY("Nathan6", 4000) == list[6]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan9", 1000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("New score", 750) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan2", 8000, false) == list[2]);	
+		CPPUNIT_ASSERT(HUENTRY("Nathan3", 7000, false) == list[3]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan6", 4000, false) == list[6]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan9", 1000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("New score", 750, true) == list[10]);
 		
 		highScores = Highscores("misc/tests/highscores/10items.high", 20);
 		list = highScores.setHighscore("Last highscore", 400, 2, 3);
 		CPPUNIT_ASSERT(list.size() == 8);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan2", 8000) == list[2]);	
-		CPPUNIT_ASSERT(HENTRY("Nathan3", 7000) == list[3]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan9", 1000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("Nathan10", 500) == list[10]);
-		CPPUNIT_ASSERT(HENTRY("Last highscore", 400) == list[11]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan2", 8000, false) == list[2]);	
+		CPPUNIT_ASSERT(HUENTRY("Nathan3", 7000, false) == list[3]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan9", 1000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan10", 500, false) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Last highscore", 400, true) == list[11]);
 		
 		highScores = Highscores("misc/tests/highscores/10items.high", 20);
 		list = highScores.setHighscore("Best highscore", 40000, 2, 15);
 		CPPUNIT_ASSERT(list.size() == 11);
-		CPPUNIT_ASSERT(HENTRY("Best highscore", 40000) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[2]);
-		CPPUNIT_ASSERT(HENTRY("Nathan2", 8000) == list[3]);	
-		CPPUNIT_ASSERT(HENTRY("Nathan3", 7000) == list[4]);
-		CPPUNIT_ASSERT(HENTRY("Nathan4", 6000) == list[5]);
-		CPPUNIT_ASSERT(HENTRY("Nathan5", 5000) == list[6]);
-		CPPUNIT_ASSERT(HENTRY("Nathan6", 4000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("Nathan9", 1000) == list[10]);
-		CPPUNIT_ASSERT(HENTRY("Nathan10", 500) == list[11]);
+		CPPUNIT_ASSERT(HUENTRY("Best highscore", 40000, true) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[2]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan2", 8000, false) == list[3]);	
+		CPPUNIT_ASSERT(HUENTRY("Nathan3", 7000, false) == list[4]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan4", 6000, false) == list[5]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan5", 5000, false) == list[6]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan6", 4000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan9", 1000, false) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan10", 500, false) == list[11]);
 		
 		highScores = Highscores("misc/tests/highscores/10items.high", 20);
 		list = highScores.setHighscore("Last highscore", 0, 7, 2);
 		CPPUNIT_ASSERT(list.size() == 11);
-		CPPUNIT_ASSERT(HENTRY("Nathan1", 10000) == list[1]);
-		CPPUNIT_ASSERT(HENTRY("Nathan2", 8000) == list[2]);	
-		CPPUNIT_ASSERT(HENTRY("Nathan3", 7000) == list[3]);
-		CPPUNIT_ASSERT(HENTRY("Nathan4", 6000) == list[4]);
-		CPPUNIT_ASSERT(HENTRY("Nathan5", 5000) == list[5]);
-		CPPUNIT_ASSERT(HENTRY("Nathan6", 4000) == list[6]);
-		CPPUNIT_ASSERT(HENTRY("Nathan7", 3000) == list[7]);
-		CPPUNIT_ASSERT(HENTRY("Nathan8", 2000) == list[8]);
-		CPPUNIT_ASSERT(HENTRY("Nathan9", 1000) == list[9]);
-		CPPUNIT_ASSERT(HENTRY("Nathan10", 500) == list[10]);
-		CPPUNIT_ASSERT(HENTRY("Last highscore", 0) == list[11]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan1", 10000, false) == list[1]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan2", 8000, false) == list[2]);	
+		CPPUNIT_ASSERT(HUENTRY("Nathan3", 7000, false) == list[3]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan4", 6000, false) == list[4]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan5", 5000, false) == list[5]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan6", 4000, false) == list[6]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan7", 3000, false) == list[7]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan8", 2000, false) == list[8]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan9", 1000, false) == list[9]);
+		CPPUNIT_ASSERT(HUENTRY("Nathan10", 500, false) == list[10]);
+		CPPUNIT_ASSERT(HUENTRY("Last highscore", 0, true) == list[11]);
 	}
 	
 	void HighscoresTest::testSave() {
