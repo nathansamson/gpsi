0c781b3b29a67acad3009d3025beae3b5c1ad71f
Nathan Samson <nathansamson@gmail.com>
2009-12-27 05:24:24 +0100

    Bunch of doc updates

--- src/game/projectile.h
+++ src/game/projectile.h
@@ -10,13 +10,30 @@
 #include "game/gameentity.h"
 
 namespace SI {
+	/**
+	 * Base class for projectiles.
+	*/
 	class IProjectile: public VGameEntity {
 	public:
+		/**
+		 * Public constructor.
+		 *
+		 * @param pos The initial position.
+		 * @param dir The direction of the projectile.
+		 * @param bshape The bounding shape of the projectile.
+		 * @param group The group of the entity.
+		 * @param fac Entity factory.
+		*/
 		IProjectile(Vector2 pos, int dir, IBoundingShapeDescription* bshape,
 	                EntityGroup* group, IGameEntityFactory* fac):
 	            VGameEntity(pos, dir, bshape, group, fac) {
 		}
 	
+		/**
+		 * Returns the impact of the projectile.
+		 *
+		 * @return the impact of the projectile.
+		*/
 		virtual double getImpact() = 0;
 	};
 }
